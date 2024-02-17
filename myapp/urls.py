2722a19c427a7a59fbc0c28952c523b4a40f78fd
Fuky9 <lukasfukan6@gmail.com>
2024-02-17 12:06:26 +0100

    Created user comment function with JS, HTML

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('blog', views.form_view, name='blog'),
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .forms import CommentForm
+from .models import UserComments
+from django.http import JsonResponse
 
-# Create your views here.
+def form_view(request):
+    form = CommentForm()
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data      # function that helps with data normalization
+            uc = UserComments(
+                first_name = cd['first_name'],
+                last_name = cd['last_name'],
+                comment = cd['comment'],
+            )
+            uc.save()
+            return JsonResponse({
+                'message': 'success'
+            })
+
+    return render(request, 'blog.html', {'form': form})
